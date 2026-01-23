b6882773e239ca1dd24713c15e79afea8514597c
Shahar Ankonina <shahar.ankonina05@gmail.com>
2026-01-23 14:29:29 -0800

    successfully trained

--- config/train_marketing_bot.py
+++ config/train_marketing_bot.py
@@ -1,34 +1,33 @@
-import time;
+# config/train_marketing_bot_small.py
+import time
 
-# Marketing bot configuration
-out_dir = 'out-marketing-bot'
-eval_interval = 200
-log_interval = 1
-eval_iters = 200
+out_dir = 'out-marketing-bot-small'
+eval_interval = 100
+log_interval = 10
+eval_iters = 50
 always_save_checkpoint = True
 
 wandb_log = False
-wandb_project = 'marketing-bot'
-wandb_run_name = 'marketing-gpt-' + str(time.time())
-# Data
+
 dataset = 'marketing_bot'
-batch_size = 16
-block_size = 512
-# Model architecture
-n_layer = 8
-n_head = 8
-n_embd = 512
+batch_size = 4
+block_size = 128
+
+# TINY model
+n_layer = 2
+n_head = 2
+n_embd = 128
 dropout = 0.1
 bias = False
-# Optimizer
-learning_rate = 3e-4
-max_iters = 5000
+
+learning_rate = 5e-4
+max_iters = 500
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0
-decay_lr = True
-warmup_iters = 100
-# Hardware
-device = 'cpu'  # or 'cuda'
+decay_lr = False
+warmup_iters = 50
+
+device = 'cpu'
 compile = False
