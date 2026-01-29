58290a0583c81d61fbd6276a9f4f7f61aef86246
Shahar Ankonina <shankonina@gmail.com>
2026-01-29 10:48:11 -0800

    The bot isnt especially smart. Might need better data or training for longer. However, the idea works!

--- sample.py
+++ sample.py
@@ -10,10 +10,10 @@ from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = 'out' # ignored if init_from is not 'resume'
+out_dir = 'out-marketing-bot' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
+num_samples = 5 # number of samples to draw
+max_new_tokens = 20 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
@@ -74,9 +74,14 @@ else:
     decode = lambda l: enc.decode(l)
 
 # encode the beginning of the prompt
-if start.startswith('FILE:'):
-    with open(start[5:], 'r', encoding='utf-8') as f:
-        start = f.read()
+# if start.startswith('FILE:'):
+#     with open(start[5:], 'r', encoding='utf-8') as f:
+#         start = f.read()
+
+print("\nNEW MESSAGE--------------------------------------------------------")
+start = "User: " + input("User: ") + "\nAssistant:"
+print("-------------------------------------------------------------------\n")
+
 start_ids = encode(start)
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
